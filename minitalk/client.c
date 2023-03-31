8f498bc291e97aa02c76df11cc7acc86ebbb4684
LeCreepyyy <42835396+LeCreepyyy@users.noreply.github.com>
2023-03-31 09:37:26 +0200

    minitalk #5
    client ok

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,11 +6,26 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 14:01:55 by vpoirot           #+#    #+#             */
-/*   Updated: 2023/03/30 13:52:50 by vpoirot          ###   ########.fr       */
+/*   Updated: 2023/03/31 09:34:47 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	bitchar(int pid, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] >> 3) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i++;
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -20,6 +35,7 @@ int	main(int argc, char **argv)
 	{
 		pid = ft_atoi(argv[1]);
 		ft_printf("Client launch.\n");
+		bitchar(pid, argv[2]);
 		return (EXIT_SUCCESS);
 	}
 	else
