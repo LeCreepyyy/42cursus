c2022841d13d5beffd4e1dcdd02d08e21f5c0d70
LeCreepyyy <42835396+LeCreepyyy@users.noreply.github.com>
2023-04-20 15:41:04 +0200

    dfs good but by gpt

--- so_long/check_map.c
+++ so_long/check_map.c
@@ -6,12 +6,12 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 09:49:42 by vpoirot           #+#    #+#             */
-/*   Updated: 2023/04/20 14:53:26 by vpoirot          ###   ########.fr       */
+/*   Updated: 2023/04/20 15:36:18 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
+/*
 int	is_possible(char **map, int x, int y, t_ft_mlx	*ft_mlx)
 {
 	if (count_item(map) == 0)
@@ -37,6 +37,51 @@ int	is_possible(char **map, int x, int y, t_ft_mlx	*ft_mlx)
 		is_possible(map, --x, y, ft_mlx);
 	}
 	return (1);
+}
+*/
+
+int	toutes_les_cases_ont_ete_visitees(char **map)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	while (map[x])
+	{
+		y = 0;
+		while (map[x][y])
+		{
+			if (map[x][y] == '0' || map[x][y] == '2')
+				return (0);
+			y++;
+		}
+		x++;
+	}
+	return (1);
+}
+
+int	is_possible(char **map, int x, int y, t_ft_mlx *ft_mlx)
+{
+	if (count_item(map) == 0 || toutes_les_cases_ont_ete_visitees(map))
+		return (1);
+	int dx[] = {0, 0, 1, -1};
+	int dy[] = {1, -1, 0, 0};
+	int i = 0;
+	while (i < 4)
+	{
+		int new_x = x + dx[i];
+		int new_y = y + dy[i];
+		if (map[new_x][new_y] != '1' && map[new_x][new_y] != '6')
+		{
+			map[new_x][new_y] = '6';
+			if (is_possible(map, new_x, new_y, ft_mlx))
+				return (1);
+			map[new_x][new_y] = '0';
+		}
+		i++;
+	}
+	return (0);
 }
 
 /*
