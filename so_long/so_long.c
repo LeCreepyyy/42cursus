442737c300ea3dc2ad431dc803abe81f183c3034
LeCreepyyy <42835396+LeCreepyyy@users.noreply.github.com>
2023-04-27 15:15:32 +0200

    so_long but ok

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 10:05:38 by vpoirot           #+#    #+#             */
-/*   Updated: 2023/04/27 10:06:19 by vpoirot          ###   ########.fr       */
+/*   Updated: 2023/04/27 13:30:54 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_hook(mlx_key_data_t keydata, void *param)
 	if (keydata.action != MLX_PRESS)
 		return ;
 	if (keydata.key == MLX_KEY_ESCAPE
-		&& ft_printf("Move : %d\nC'est perdu !\n", ft_mlx->moov - 1))
+		&& ft_printf("Move : %d\n", ft_mlx->moov - 1))
 		mlx_close_window(ft_mlx->mlx);
 	if ((keydata.key == MLX_KEY_UP || keydata.key == MLX_KEY_W)
 		&& ft_action(ft_mlx, (ft_mlx->player_p[0] - 1), ft_mlx->player_p[1])
@@ -87,6 +87,7 @@ int	main(void)
 	mlx_key_hook(ft_mlx->mlx, &ft_hook, ft_mlx);
 	mlx_loop(ft_mlx->mlx);
 	mlx_terminate(ft_mlx->mlx);
-	free(ft_mlx);
-	return (0);
+	free_tab(ft_mlx->map);
+	free_tab(tab);
+	return (free(ft_mlx->player_p), free(ft_mlx), 0);
 }
--- so_long/so_long_utils.c
+++ so_long/so_long_utils.c
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 10:08:22 by vpoirot           #+#    #+#             */
-/*   Updated: 2023/04/26 15:26:38 by vpoirot          ###   ########.fr       */
+/*   Updated: 2023/04/27 13:58:23 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,10 +66,36 @@ char	**dup_map(char **map)
 	return (new);
 }
 
+void	print_map(char	**map)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			ft_printf("%c", map[y][x]);
+			x++;
+		}
+		ft_printf("\n");
+		y++;
+	}
+}
+
 int	ft_action(t_ft_mlx *ft_mlx, int y, int x)
 {
 	if (ft_mlx->map[y][x] == '1')
 		return (0);
+	if (ft_mlx->map[y][x] == 'C')
+		ft_mlx->map[y][x] = '0';
+	if (ft_mlx->map[y][x] == 'E')
+	{
+		if ((count_item(ft_mlx->map) - 1) == 0)
+			mlx_close_window(ft_mlx->mlx);
+	}
 	ft_printf("Move : %d\r", ft_mlx->moov++);
 	return (1);
 }
