a250f8b2b98a5e0752fe577f196dbd47c8c90615
LeCreepyyy <42835396+LeCreepyyy@users.noreply.github.com>
2023-05-24 16:28:33 +0200

    refresh

--- push_swap/big_algo.c
+++ push_swap/big_algo.c
@@ -6,7 +6,7 @@
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 15:19:38 by vpoirot           #+#    #+#             */
-/*   Updated: 2023/05/23 15:02:58 by vpoirot          ###   ########.fr       */
+/*   Updated: 2023/05/24 15:52:52 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,29 @@ int	find_nbr(int *stack, int limit)
 	return (0);
 }
 
+int	nbr_is_up(int *a, int split)
+{
+	int	i;
+
+	i = 0;
+	while (a[i] < (lenstack(a) / 2))
+	{
+		if (a[i] < split)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	spliter(int	*b, int add, int split)
+{
+	int	limit;
+
+	limit = split - (add / 2);
+	if (b[0] > limit)
+		rotate(b, 'b');
+}
+
 void	big_sort(int *a, int *b, int package)
 {
 	int	split;
@@ -38,12 +61,19 @@ void	big_sort(int *a, int *b, int package)
 	while (split <= size)
 	{
 		if (a[0] < split)
+		{
 			push(b, a, 'b');
-		else
+			spliter(b, add, split);
+		}
+		else if (!nbr_is_up(a, split))
 			rotate(a, 'a');
+		else
+			r_rotate(a, 'a');
 		if (!find_nbr(a, split))
 			split += add;
 	}
+	while (a[0])
+		push(b, a, 'b');
 	while (b[0])
 		get_push_b(b, a);
 }
