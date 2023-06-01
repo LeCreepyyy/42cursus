8fed2b0fa16d18397539b15200ca507dc035c96c
LeCreepyyy <42835396+LeCreepyyy@users.noreply.github.com>
2023-06-01 22:49:38 +0200

    checker else if ok

--- push_swap/checker.c
+++ push_swap/checker.c
@@ -41,6 +41,41 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	return (0);
 }
 
+void	grep_move(int *a, int *b)
+{
+	if (ft_strncmp("sa\n", get_next_line(STDIN_FILENO), 3) == 0)
+		s(a);
+	else if (ft_strncmp("sb\n", get_next_line(STDIN_FILENO), 3) == 0)
+		s(b);
+	else if (ft_strncmp("pa\n", get_next_line(STDIN_FILENO), 3) == 0)
+		p(a, b);
+	else if (ft_strncmp("pb\n", get_next_line(STDIN_FILENO), 3) == 0)
+		p(b, a);
+	else if (ft_strncmp("ra\n", get_next_line(STDIN_FILENO), 3) == 0)
+		r(a);
+	else if (ft_strncmp("rb\n", get_next_line(STDIN_FILENO), 3) == 0)
+		r(b);
+	else if (ft_strncmp("ss\n", get_next_line(STDIN_FILENO), 3) == 0)
+	{
+		s(a);
+		s(b);
+	}
+	else if (ft_strncmp("rr\n", get_next_line(STDIN_FILENO), 3) == 0)
+	{
+		r(a);
+		r(b);
+	}
+	else if (ft_strncmp("rra\n", get_next_line(STDIN_FILENO), 3) == 0)
+		r_r(a);
+	else if (ft_strncmp("rrb\n", get_next_line(STDIN_FILENO), 3) == 0)
+		r_r(b);
+	else if (ft_strncmp("rrr\n", get_next_line(STDIN_FILENO), 3) == 0)
+	{
+		r_r(a);
+		r_r(b);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int		*a;
@@ -56,9 +91,6 @@ int	main(int argc, char **argv)
 	a = create_a(argc, argv);
 	b = create_b(argc);
 	while (is_sort(a) == 1 && b[0] == 0)
-	{
-		if (ft_strncmp("sa\n", get_next_line(STDIN_FILENO), 3) == 0)
-			s(a);
-	}
+		grep_move(a, b);
 	exit(EXIT_SUCCESS);
 }
