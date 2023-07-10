d5a2b245704ea15c71f5f194bd179f56800bbaa6
LeCreepyyy <42835396+LeCreepyyy@users.noreply.github.com>
2023-07-10 15:04:14 +0200

    fclean && add ms

--- ms/src/utils.c
+++ ms/src/utils.c
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/23 17:10:16 by vpoirot           #+#    #+#             */
-/*   Updated: 2023/06/26 15:51:14 by vpoirot          ###   ########.fr       */
+/*   Created: 2023/06/26 13:35:30 by vpoirot           #+#    #+#             */
+/*   Updated: 2023/06/26 13:38:42 by vpoirot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../include/minishell.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <signal.h>
-# include <unistd.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
 
-// fonction utils :
-int		ft_strcmp(char *s1, char *s2);
-
-//other
-void	init_signal(void);
-
-#endif
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	if (s1[i] != s2[i])
+		return (1);
+	return (0);
+}
